a3b5a34871b0660a140d323b72e842fadd6ed482
mininim <minsoplee2015@gmail.com>
2023-05-14 04:29:36 +0900

    해금안댐돌아가 기능 추가

--- podong4cuts/podong4cuts/src/Team1_Map/View/FilterView/FilterView.swift
+++ podong4cuts/podong4cuts/src/Team1_Map/View/FilterView/FilterView.swift
@@ -13,6 +13,8 @@ struct FilterListView: View {
     @ObservedObject var VM: PodongViewModel
     @State var filterNumber: Int = 0
     
+    @State private var showingBackAlert = false
+    
     let columns: [GridItem] = [
         GridItem(.flexible(), spacing: 0, alignment: nil ),
         GridItem(.flexible(), spacing: 0, alignment: nil )]
@@ -48,11 +50,18 @@ struct FilterListView: View {
                     }//】 VStack
                     .onTapGesture {
                         //TODO: 장소별 필터뷰로 이동하는 코드
-                        print("Tapped")
-                        filterNumber = index.number
-                        print(filterNumber)
-                        showDefaultCameraFrameView = true
-                        print(showDefaultCameraFrameView)
+                        if VM.spotdata[index.number].isOpened{
+                            filterNumber = index.number
+                            showDefaultCameraFrameView = true
+                        }else{
+                            showingBackAlert = true
+                        }
+                        
+                    }
+                    .alert(isPresented: $showingBackAlert) {
+                        
+                        Alert(title: Text("필터가 아직 해금되지 않았습니다"), message: Text("해당 스팟에 가서 위치 인증 후 사용할 수 있습니다."), dismissButton: .default(Text("확인")))
+                        
                     }
                 }//】 Loop
             }//】 Grid
