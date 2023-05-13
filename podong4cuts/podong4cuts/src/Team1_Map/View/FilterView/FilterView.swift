8dde8fff10eca8a3876a689a13b27b76941a0ae2
Lets <ruvan123@naver.com>
2023-05-14 03:35:06 +0900

    Feat: Link FilterList with each Compatible CameraFrameView

--- podong4cuts/podong4cuts/src/Team1_Map/View/FilterView/FilterView.swift
+++ podong4cuts/podong4cuts/src/Team1_Map/View/FilterView/FilterView.swift
@@ -11,80 +11,54 @@ struct FilterListView: View {
     
     //property
     @ObservedObject var VM: PodongViewModel
-    var filterNumber: Int = 0
+    @State var filterNumber: Int = 0
     
     let columns: [GridItem] = [
         GridItem(.flexible(), spacing: 0, alignment: nil ),
         GridItem(.flexible(), spacing: 0, alignment: nil )]
     
+    @State private var showDefaultCameraFrameView = false
     
     var body: some View {
         
-            
+        NavigationView {
             LazyVGrid(columns: columns) {
-//                VStack{
-//                    Image("spacewalk-1")
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(width: 170, height: 180)
-//                        .cornerRadius(15)
-//                    Text("스페이스워크")
-//                }
-//
-//
-//                VStack{
-//                    Image("tomatilo-1")
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(width: 170, height: 180)
-//                        .cornerRadius(15)
-//                    Text("토마틸로")
-//                }
-//
-//                VStack{
-//                    Image("bridge-1")
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(width: 170, height: 180)
-//                        .cornerRadius(15)
-//                    Text("영일교")
-//                }
-//
-//                VStack{
-//                    Image("whale-1")
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(width: 170, height: 180)
-//                        .cornerRadius(15)
-//                    Text("고래꼬리워터폴리")
-//                }
-                
                 ForEach(VM.spotdata){ index in
                     VStack{
                         //if
                         ZStack{
                             Color.gray.opacity(0.5)
-
+                            
                             Image(systemName: "lock.fill")
                                 .font(.title)
                                 .foregroundColor(.black.opacity(0.3))
-
+                            
                             Image(index.cover)
                                 .resizable()
                                 .scaledToFill()
                                 .opacity(index.isOpened ? 1.0 : 0)
-
+                            
                         }//】 ZStack
                         .frame(width: 170, height: 180)
                         .cornerRadius(15)
                         Text(index.name)
+                        NavigationLink("", isActive: $showDefaultCameraFrameView) {
+                            DefaultCameraFrameView(selected: filterNumber)
+                        }
                     }//】 VStack
                     .onTapGesture {
                         //TODO: 장소별 필터뷰로 이동하는 코드
+                        print("Tapped")
+                        filterNumber = index.number
+                        print(filterNumber)
+                        showDefaultCameraFrameView = true
+                        print(showDefaultCameraFrameView)
                     }
                 }//】 Loop
             }//】 Grid
             .padding(.horizontal)
+            
+        }// NavigationView
         
     }//】 Body
 }
