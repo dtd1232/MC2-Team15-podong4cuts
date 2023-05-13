47e7072ab82c06607927838e11957906312823b4
Eric <real.dassy@gmail.com>
2023-05-13 20:12:06 +0900

    FEAT: WhaleTailWhaleARView 영일대에서 표시되는 방향 수정

---
+++ podong4cuts/podong4cuts/src/Team2_AR/View/WhaleTailWhaleARView.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  ARCL3
+//
+//  Created by Song Jihyuk on 2023/05/08.
+//
+
+import ARKit_CoreLocation
+import ARKit
+import CoreLocation
+import SwiftUI
+
+struct WhaleTailWhaleARView: UIViewRepresentable {
+    let sceneLocationView = SceneLocationView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        
+        let locationManager = CLLocationManager()
+        locationManager.startUpdatingLocation()
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.frameSemantics = .personSegmentationWithDepth
+//        configuration.planeDetection = .horizontal
+        configuration.worldAlignment = .gravityAndHeading
+        sceneLocationView.session.run(configuration)
+        sceneLocationView.autoenablesDefaultLighting = true
+
+        // MARK: 임시 고래꼬리 좌표
+//        let location = CLLocation(coordinate: CLLocationCoordinate2D(latitude: locationManager.location!.coordinate.latitude, longitude: locationManager.location!.coordinate.longitude), altitude: locationManager.location!.altitude)
+        
+
+        let location = CLLocation(coordinate: CLLocationCoordinate2D(latitude: 36.05282, longitude: 129.37828), altitude: 30000)
+        let locationNode = LocationNode(location: location)
+
+        
+//        let usdzURL = Bundle.main.url(forResource: "WhaleAR", withExtension: "usdz")
+        let usdzURL = Bundle.main.url(forResource: "WhaleAR", withExtension: "usdz")
+        let scene = try! SCNScene(url: usdzURL!, options: [.checkConsistency: true])
+        
+        let node = scene.rootNode.childNodes[0]
+        
+        locationNode.addChildNode(node)
+        locationNode.continuallyUpdatePositionAndScale = false
+        locationNode.continuallyAdjustNodePositionWhenWithinRange = false
+        locationNode.scaleRelativeToDistance = true
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: locationNode)
+        
+        return sceneLocationView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
