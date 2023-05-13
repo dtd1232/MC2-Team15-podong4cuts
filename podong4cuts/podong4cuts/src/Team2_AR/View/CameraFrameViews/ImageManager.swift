4b1f48f2925bc198f20741661084f68630a8b89d
Lets <ruvan123@naver.com>
2023-05-13 19:55:41 +0900

    Feat: CameraFrameView

--- podong4cuts/podong4cuts/src/Team2_AR/View/CameraFrameViews/ImageManager.swift
+++ podong4cuts/podong4cuts/src/Team2_AR/View/CameraFrameViews/ImageManager.swift
@@ -1,25 +1,28 @@
 //
 //  ImageManager.swift
-//  podong4cuts
+//  CameraViewFinal
 //
-//  Created by 이승용 on 2023/05/12.
+//  Created by user on 2023/05/13.
 //
 
 import Photos
 import UIKit
 
-class ImageManager {
+class ImageManager:ObservableObject {
     static let shared = ImageManager()
     
     private let imageManager = PHImageManager()
     var requestImageOption = PHImageRequestOptions()
+    
+    var successHandler: ((UIImage) -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
     
     init() {
         setRequestImageOptions()
     }
     
     func requestImage(from asset: PHAsset, thumbnailSize: CGSize, completion: @escaping (UIImage?) -> Void) {
-        
         self.imageManager.requestImage(for: asset, targetSize: CGSize(width: 120, height: 120), contentMode: .aspectFill, options: requestImageOption) { image, info in
             completion(image)
         }
@@ -33,7 +36,31 @@ class ImageManager {
     
     func setRequestImageOptions() {
         requestImageOption.isSynchronous = true
-        requestImageOption.deliveryMode = PHImageRequestOptionsDeliveryMode.highQualityFormat
+        requestImageOption.deliveryMode = .highQualityFormat
         requestImageOption.resizeMode = PHImageRequestOptionsResizeMode.exact
     }
+    
+    func saveSnapShot(snapShot: UIImage, completion: @escaping () -> Void) {
+        UIImageWriteToSavedPhotosAlbum(snapShot, nil, #selector(saveCompleted), nil)
+        completion()
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+            if let error = error {
+                errorHandler?(error)
+            } else {
+                successHandler?(image)
+
+            }
+        }
+    
+    
+    func loadPHAsset(completion: @escaping (PHFetchResult<PHAsset>) -> Void) {
+        let fetchOption = PHFetchOptions()
+        fetchOption.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        let fetchPhotos = PHAsset.fetchAssets(with: fetchOption)
+        completion(fetchPhotos)
+    }
 }
+
+
